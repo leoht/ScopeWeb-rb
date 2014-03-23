f507f4d2cdab000723f3adcae2cb971a8ce48086
Leo Hetsch <leo.hetsch@gmail.com>
2014-03-23 22:01:01 +0100

    Server

--- server/Association.rb
+++ server/Association.rb
@@ -85,7 +85,8 @@ module Scope
           message = Message.new
           message.direction = MessageTypes::BROADCAST
           message.name = MessageTypes::NOTICE_AT_TIMECODE
-          message.data = { :title => notice.title, :content => notice.content, :timecode => notice.timecode, :short_content => notice.short_content, :image => notice.image, :category => notice.parent.title, :category_nicename => notice.parent.nicename }
+          # message.data = { :id => notice.id, :title => notice.title, :content => notice.content, :timecode => notice.timecode, :short_content => notice.short_content, :image => notice.image, :category => notice.parent.title, :category_nicename => notice.parent.nicename }
+          message.data = { :id => notice.id, :title => notice.title, :short_content => notice.short_content, :category_nicename => 'impact' }
           puts " [x] Sending notice for timecode : " + message.get('timecode').to_s
         end
       end

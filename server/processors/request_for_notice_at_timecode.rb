81aaac9431e5802f9f1ab5b7f05f3cb31531cdef
Leo Hetsch <leo.hetsch@gmail.com>
2014-03-26 19:16:01 +0100

    Fix server message for notice at timecode

--- server/processors/request_for_notice_at_timecode.rb
+++ server/processors/request_for_notice_at_timecode.rb
@@ -11,7 +11,7 @@ module Scope
           answer.direction = MessageTypes::BROADCAST
           answer.name = MessageTypes::NOTICE_AT_TIMECODE
           # message.data = { :id => notice.id, :title => notice.title, :content => notice.content, :timecode => notice.timecode, :short_content => notice.short_content, :image => notice.image, :category => notice.parent.title, :category_nicename => notice.parent.nicename }
-          answer.data = { :id => notice.id.to_s, :title => notice.title, :short_content => notice.short_content, :category_nicename => 'impact' }
+          answer.data = { :timecode => message.get('timecode').to_s, :id => notice.id.to_s, :title => notice.title, :short_content => notice.short_content, :category_nicename => 'impact' }
           puts " [x] Sending notice for timecode : " + message.get('timecode').to_s
           puts answer.data.inspect
         end

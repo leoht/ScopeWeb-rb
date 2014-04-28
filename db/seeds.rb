64d895dff6e8ee08257fc7d083f7b8258015350d
Leo Hetsch <leo.hetsch@gmail.com>
2014-04-28 18:51:35 +0200

    Website Scope

--- db/seeds.rb
+++ db/seeds.rb
@@ -57,9 +57,10 @@ secrets_category  = NoticeCategory.create(title: 'Secrets de tournage', nicename
 deshumanisation_category  = NoticeCategory.create(title: 'Déshumanisation', nicename: 'themes_deshumanisation', parent: themes_category)
 others_category  = NoticeCategory.create(title: 'Autres', nicename: 'themes_others', parent: themes_category)
 
+scope_category = NoticeCategory.create(title: 'Scope', nicename: 'scope', color: 'fff')
 # SEED NOTICES
 
-movie.notices.create(title: 'Bienvenue', timecode: '10', short_content: 'Ici s\'afficheront les notices documentaires de votre film. Vous pouvez défiler sur la ligne temporelle pour retrouver celles que vous avez déjà vues ou ratées. Rendez-vous à la fin du film, bon visionnage !', content: '', category: cinema_category)
+movie.notices.create(title: 'Bienvenue', timecode: '10', short_content: 'Ici s\'afficheront les notices documentaires de votre film. ', content: '', category: scope_category)
 
 # IMPACT / CINEMA
 movie.notices.create(title: 'Blade Runner', timecode: '1382', end_timecode: '1403', short_content: 'Le commissariat de Blade Runner est la copie conforme (angle de vue compris) d’une des tours de Metropolis.', content: '', category: cinema_category)
